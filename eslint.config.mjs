e2678ab7a446d098d853ae623f1e01ad553a2bbf
cronicasfootselfie-sys <cronicas.footselfie@oficinas-upch.pe>
2025-11-20 08:43:53 -0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,25 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Config básica de Next + TypeScript
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Reglas globales adicionales
+  {
+    rules: {
+      // Evita el error de las comillas en JSX
+      "react/no-unescaped-entities": "off",
+    },
+  },
+
+  // Reglas específicas para un archivo
+  {
+    files: ["src/lib/redcapFirestore.ts"],
+    rules: {
+      // Evita el error de "Unexpected any"
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
